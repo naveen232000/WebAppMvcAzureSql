e80f028c85b1255d4a203a6d6d75a7bca52eddfe
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-22 16:17:58 +0530

    Add project files.

---
+++ WebAppMvcAzureSql/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppMvcAzureSql.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string? Fname { get; set; }
+        public string? Lname { get; set; }
+        public double? Salary { get; set; }
+        public DateTime? Doj { get; set; }
+    }
+}
---
+++ WebAppMvcAzureSql/Models/sqldbfib22Context.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAppMvcAzureSql.Models
+{
+    public partial class sqldbfib22Context : DbContext
+    {
+        public sqldbfib22Context()
+        {
+        }
+
+        public sqldbfib22Context(DbContextOptions<sqldbfib22Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:navi23server.database.windows.net,1433;Initial Catalog=sqldbfib22;Persist Security Info=False;User ID=admin123;Password=admin@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Doj)
+                    .HasColumnType("date")
+                    .HasColumnName("DOJ");
+
+                entity.Property(e => e.Fname).HasMaxLength(50);
+
+                entity.Property(e => e.Lname).HasMaxLength(50);
+
+                entity.Property(e => e.Salary).HasColumnName("salary");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
